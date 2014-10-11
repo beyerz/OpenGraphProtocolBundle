3de13b0d82ae05e3276c173210e9c3a530427a09
Lance Bailey <bailz777@gmail.com>
2014-10-11 20:04:44 +0300

    added dynamic setter interface

--- DependencyInjection/OpenGraphSetter.php
+++ DependencyInjection/OpenGraphSetter.php
@@ -1,23 +1,23 @@
 <?php
-namespace Beyerz\Bundle\OpenGraphProtocolBundle\DependencyInjection;
-use Beyerz\Bundle\OpenGraphProtocolBundle\Libraries\OpenGraph;
+namespace Beyerz\OpenGraphProtocolBundle\DependencyInjection;
+
+use Beyerz\OpenGraphProtocolBundle\DependencyInjection\OpenGraphSetterInterface;
+use Beyerz\OpenGraphProtocolBundle\Libraries\OpenGraph;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 /**
  * @author Lance
  *
  */
-interface OpenGraphSetter {
-	/**
-	 * Use this function to set open graph attributes based on the event<br>
-	 * No return is required, simply update the object<br>
-	 * Setting an already set attribute will override the default that<br>
-	 * was set in the config.
-	 * @tutorial
-	 * 				$base = $openGraph->getLoadedLib('base');<br>
-					$base->addMeta('description','Sample description text');<br>
+class OpenGraphSetter implements OpenGraphSetterInterface {
 
-	 * @param OpenGraph $openGraph
-	 * @param GetResponseEvent $event
+	/* (non-PHPdoc)
+	 * @see \Beyerz\OpenGraphProtocolBundle\DependencyInjection\OpenGraphSetter::dynamicSet()
 	 */
-	public function dynamicSet(OpenGraph $openGraph, GetResponseEvent $event);
+	public function dynamicSet(OpenGraph $openGraph, GetResponseEvent $event) {
+
+		$base = $openGraph->getLoadedLib('base');
+		$base->addMeta('description','Sample description text');
+		echo "<pre>";
+		var_dump($event->getRequest(), $event->getResponse());die();
+	}
 }
